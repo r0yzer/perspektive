7e9cd800b7ca5736c33fed16ad2ca9626faf9ffe
Jakob K <dev@jakobk.de>
2022-06-06 15:22:37 +0200

    Update to 1.19 and apply Kotlin style guide

--- src/main/java/de/royzer/perspektive/mixins/CameraMixin.java
+++ src/main/java/de/royzer/perspektive/mixins/CameraMixin.java
@@ -18,13 +18,13 @@ public abstract class CameraMixin {
     protected abstract void setRotation(float yaw, float pitch);
 
     @Inject(
-            method = "setup",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/Camera;setRotation(FF)V",
-                    shift = At.Shift.AFTER,
-                    ordinal = 0
-            )
+        method = "setup",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/Camera;setRotation(FF)V",
+            shift = At.Shift.AFTER,
+            ordinal = 0
+        )
     )
     public void update(BlockGetter area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo ci) {
         if (Perspektive.INSTANCE.getFreeLookEnabled()) {
